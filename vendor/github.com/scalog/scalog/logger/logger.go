fa630faf0d6c4a504766c0f17355a3ce8d8a428f
Cong Ding <tuxding@gmail.com>
2019-09-05 17:41:25 -0400

    vendor: init

---
+++ vendor/github.com/scalog/scalog/logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+var logger *log.Logger
+
+func init() {
+	logger = log.New(os.Stdout, "[scalog] ", log.Ldate|log.Lmicroseconds)
+}
+
+func Printf(format string, v ...interface{}) {
+	logger.Printf(format, v...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	logger.Printf(format, v...)
+}
+
+func Infof(format string, v ...interface{}) {
+	logger.Printf(format, v...)
+}
+
+func Warningf(format string, v ...interface{}) {
+	logger.Printf(format, v...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	logger.Printf(format, v...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	logger.Fatalf(format, v...)
+}
+
+func Panicf(format string, v ...interface{}) {
+	logger.Panicf(format, v...)
+}
